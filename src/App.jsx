df825e4e4bc27ee8672416751a0ddcf2f5cc8cf2
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 23:21:59 -0500

    estilos para que no esten muy pegadas las cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
     return (
-        <>
+        <section className="App">
             <TwitterFollowCard userName="ferrari" firstName="Jhonatan Isai" />
             <TwitterFollowCard userName="mouredev" firstName="Moure Dev" />
-        </>
+        </section>
     )
 }
