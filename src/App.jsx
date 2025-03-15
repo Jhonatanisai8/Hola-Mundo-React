06f6236fec8b752e445a825aeee3c6d43d6d4348
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 23:06:18 -0500

    llamando al componente en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
 export function App() {
     return (
-        <article className="tw-followCard">
-            <header className="tw-followCard-header">
-                <img className="tw-followCard-avatar" src="https://unavatar.io/ferrari" alt="Ferrari" />
-                <div className="tw-followCard-info">
-                    <strong>Jhoonatan Isai</strong>
-                    <span className="tw-followCard-infoUserName">@JhonatanIsai</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className="tw-followCard-button">
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <>
+            <TwitterFollowCard userName="ferrari" firstName="Jhonatan Isai" />
+            <TwitterFollowCard userName="mouredev" firstName="Moure Dev" />
+        </>
     )
 }
